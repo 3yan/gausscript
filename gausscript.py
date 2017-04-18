9d87f0c78b3c4bc0996ea74fbe5e49def93cc8b0
Yan <3yanyanyan@gmail.com>
2017-04-18 06:30:34 +0200

    Added batch-mode feature and fixed minor bugs of interactive mode, also fixed output checking for missing lines and fixed routecard line problem, still few problems with MarvinSketch .xyz format

--- gausscript.py
+++ gausscript.py
@@ -13,10 +13,10 @@ import os
 clinput = argparse.ArgumentParser()
 #we want file as geometry input
 clinput.add_argument('-i', '--input', '--inputgeom', required=True,
-        help='input geometry in gaussian format (for example Molden ZMat output)', type=file)
+        help='input geometry in gaussian format (for example Molden ZMat output), when in batch mode it must be folder containing files of type mentioned above', type=str)
 #we want filename to which we will write
 clinput.add_argument('-o', '--outfile',
-        help="""output file in which complete g09 input is fully defined and ready to be calculated. It's requied to be in correct format (*.gjf or *.com). If no file is defined it will automatically create file with same name to input file with .gjf suffix in current working directory""", type=str)
+        help="""output file in which complete g09 input is fully defined and ready to be calculated. It's requied to be in correct format (*.gjf or *.com). If no file is defined it will automatically create file with same name to input file with .gjf suffix in current working directory, if running in batch-mode this argument will be ignored and input filename with gjf suffix will be used instead""", type=str)
 clinput.add_argument('-c', '--charge',
         help='charge of structure', type=int)
 clinput.add_argument('-m', '--multiplicity',
@@ -27,6 +27,7 @@ clinput.add_argument('-M', '--memory', type=int,
         help='amount of ram (MB)')
 clinput.add_argument('-N', '--nproc', type=int,
         help='number of processor')
+clinput.add_argument('-b', '--batch', action='store_true', help='run command in a batch mode (you must cast it on folder which contains files instead on file, for easiness of operation outfile command would be ignored and INPUT.gjf format for output files will be used instead)')
 args = clinput.parse_args()
 
 
@@ -34,34 +35,91 @@ args = clinput.parse_args()
 #predefined route card list 
 predefrc = [
           "#AM1 opt freq pop=full"
-        , "#B3LYP/cc-pVDZ opt freq pop=full"
+        , "#B3LYP/def2-SVP opt pop=full"
+        , "#B3LYP/def2-SVP opt freq pop=full"
         ]
 
 
-
+jobname = []
+inputgeom = []
+outfile = []
 
 
 #processing arguments:
 #read input geometry to memory and close it (https://docs.python.org/2/tutorial/inputoutput.html and for details https://docs.python.org/2/library/stdtypes.html#file-objects )
-inputgeom = args.input.read()
-args.input.close()
-
-#check that outputfile is in a good format
-if args.outfile == None:
-    args.outfile = (os.path.basename(args.input.name) + ".gjf")
+if args.batch == False:
+    filepath="."
+    try:
+        inputfile = open(args.input,'r')
+        inputgeom.append(inputfile.read())
+        inputfile.close()
+        jobname.append(args.input)
+    except:
+        print """\n\n\n Input {} is not file, exiting...""".format(args.input)
+        exit(0)
 else:
-    outsuffix = args.outfile[-4:]
-    if not (outsuffix == ".gjf" or outsuffix == ".com"):
-        print "\n\n\n          !!!ERROR!!! INCORRECT FORMAT OF OUTPUT FILE USED PLEASE SEE HELP FOR DETAILS\n\n\n\n"
-        clinput.print_help()
-        exit(1)
+    #if it's batch input we generate all what looks save to generate and skip wierdnesses
+    try:
+        inputfiles = os.listdir(args.input)
+        print """\n\n\n"""
+    except:
+        print """\n\n\n Input {} is not folder, exiting...""".format(args.input)
+        exit(0)
+    for i in inputfiles:
+        filepath = args.input + "/" + i
+        #need to process .xyz geometry exception..
+        insuffix = i[-4:]
+        if (insuffix == ".gjf" or insuffix == ".com"):
+            print """Skipping {} - looks like output file...""".format(filepath)
+        elif os.path.isdir(filepath):
+            print """Skipping {} - looks like directory...""".format(filepath)
+        elif os.path.exists(filepath + ".gjf"):
+            print """Skipping {0} - looks like already have it's counterpart {0}.gjf...""".format(filepath)
+        elif insuffix == ".xyz" and os.path.exists(filepath[:-4] + ".gjf"):
+            print """Skipping {0}.xyz - looks like already have it's counterpart {0}.gjf...""".format(filepath[:-4])
+        else:
+            try:
+                # END OF HERE WORKING ...
+                inputfile = open(filepath,'r')
+                inputgeom.append(inputfile.read())
+                inputfile.close()
+                jobname.append(i)
+                print """Adding {} to input array...""".format(filepath)
+            except:
+                print """{} is not proper file, skipping...""".format(filepath)
+    jobnames = """{}""".format(jobname)
+    print """listing DONE, will process files {} from directory {}""".format(jobnames[1:-1], args.input)
 
-#if outputfile already exists ask if we should overwrite it (https://docs.python.org/2/library/os.path.html#os.path.exists, https://docs.python.org/2/library/stdtypes.html#str.format)
-if os.path.exists(args.outfile):
-    owerwrite = raw_input("""Output file {} already exists in filesystem, overwrite it? [y/N]: """.format(args.outfile))
-    if owerwrite != 'y':
+#if input is in .xyz format, remove suffix in case of jobname
+for i,j in enumerate(jobname):
+    name = j
+    insuffix = j[-4:]
+    if insuffix == ".xyz":
+        jobname[i] = name[:-4]
+
+#check that outputfile is in a good format If not running in batch mode
+if args.batch == False:
+    if args.outfile == None:
+        outfile.append(os.path.basename(jobname[0]) + ".gjf")
+    else:
+        outsuffix = args.outfile[-4:]
+        if not (outsuffix == ".gjf" or outsuffix == ".com"):
+            print "\n\n\n          !!!ERROR!!! INCORRECT FORMAT OF OUTPUT FILE USED PLEASE SEE HELP FOR DETAILS\n\n\n\n"
+            clinput.print_help()
+            exit(1)
+        outfile.append(outfile[0])
+    #if outputfile already exists ask if we should overwrite it (https://docs.python.org/2/library/os.path.html#os.path.exists, https://docs.python.org/2/library/stdtypes.html#str.format)
+    if os.path.exists(outfile[0]):
+        owerwrite = raw_input("""Output file '{}' already exists in filesystem, overwrite it? [y/N]: """.format(outfile[0]))
+        if owerwrite != 'y':
             print "\n\n\nNot overwriting already existing gaussian input file, exiting..."
             exit(0)
+
+#generate output file names if running in batch mode
+else:
+    for i in jobname:
+        outfile.append(os.path.basename(i) + ".gjf")
+
 
 #if charge, multiplicity or number of cpus is not defined, define them (and restrict input to integer)
 for i in ["charge", "multiplicity", "nproc"]:
@@ -69,8 +127,12 @@ for i in ["charge", "multiplicity", "nproc"]:
         #https://docs.python.org/3/reference/compound_stmts.html#try
         infiniteloop = True
         while infiniteloop:
+            rawinput = (raw_input("\n\n\n Please define {} (or x to exit): ".format(i)))
+            if rawinput == 'x' :
+                print("x pressed, exiting...")
+                exit(0)
             try:
-                setattr(args, i, int(raw_input("\n\n\n Please define {}: ".format(i))))
+                setattr(args, i, int(rawinput))
                 infiniteloop = False
             except:
                 print("excepted integer, please try to concentrate and write some nice integer when trying to set {}".format(i))
@@ -81,8 +143,12 @@ for i in ["charge", "multiplicity", "nproc"]:
 if args.memory == None:
     infiniteloop = True
     while infiniteloop:
+        rawinput = (raw_input("\n\n\n Please define memory (MB) (or x to exit): ".format(i)))
+        if rawinput == 'x' :
+            print("x pressed, exiting...")
+            exit(0)
         try:
-            args.memory = int(raw_input("\n\n\n Please define memory (MB): "))
+            args.memory = int(rawinput)
             infiniteloop = False
         except:
             print("excepted integer, please try to concentrate and write some nice integer when trying to set Memory")
@@ -90,7 +156,9 @@ print ("""Memory is set to {} MB\n""".format(args.memory))
 
 #if head file is defined read it, if not, just show our predefined heads..
 if not (args.routecard == None):
-    routecard = args.routecard.read()
+    routecard = args.routecard.readline()
+    if routecard[-1:] == """\n""":
+        routecard = routecard[:-1]
     args.routecard.close()
 
 #enumerate predefined heads (https://docs.python.org/2/library/functions.html#enumerate) and list them
@@ -101,7 +169,7 @@ else:
     headnum = int(raw_input("\n Please select number of route card: "))-1
     print("")
     routecard = predefrc[headnum]
-print ("""Route card is set to:\n{}""".format(routecard))
+print ("""Route card is set to:\n'{}'""".format(routecard))
 
 
 
@@ -109,22 +177,33 @@ print ("""Route card is set to:\n{}""".format(routecard))
 
 
 #we've everything what we need, so it's time to roll a dice
-writeit = str(raw_input("""\n\n\n    All criterias was defined, write gaussian input into file "{}" ? [Y/n]""".format(args.outfile)))
+outfilelist = """{}""".format(outfile)
+writeit = str(raw_input("""\n\n\n    All criterias was defined, write gaussian input into file(s) {} ? [Y/n]""".format(outfilelist[1:-1])))
 
 if (writeit == 'n' or writeit == 'N' or writeit == 'No' or writeit == 'no'):
-    print ("""\n\n\ncreation of configuration file "{}" canceled, exiting...""".format(args.outfile))
+    print ("""\n\n\ncreation of configuration file(s) {} canceled, exiting...""".format(outfilelist[1:-1]))
     exit(0)
 
-print ("""creating/rewriting configuration file "{}" """.format(args.outfile))
+print ("""creating/rewriting configuration file(s) {} """.format(outfilelist[1:-1]))
 
-outfile = open(args.outfile, 'w')
-outfile.write("""%chk={}
+for i,j in enumerate(outfile):
+    if args.batch == False:
+        setattr(args,"input",".") 
+    filepath = args.input + "/" + j
+    outfile = open(filepath,'w')
+    geometry = inputgeom[i]
+    #need empty line on end of gjf file, otherwise gaussian is likely to fail
+    if geometry[-2:] != """\n\n""":
+        geometry = geometry + """\n"""
+    outfile.write(
+"""%chk={}
 %mem={}MB
 %nproc={}
 {}
 
+
 {} {}
-{}""".format((os.path.basename(args.outfile))[:-4] + ".chk", args.memory, args.nproc, routecard, args.charge, args.multiplicity, inputgeom))
-outfile.close
+{}""".format(j[:-4] + ".chk", args.memory, args.nproc, routecard, args.charge, args.multiplicity, geometry))
+    outfile.close
 
 exit(0)
